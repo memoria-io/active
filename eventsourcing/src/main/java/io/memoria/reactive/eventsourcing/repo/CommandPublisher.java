c8305f0783fa94842f0b5f614551aca36fa8be9e
Ismail Marmoush <marmoushismail@gmail.com>
2023-11-05 17:46:35 +0100

    removing eventsourcing

--- eventsourcing/src/main/java/io/memoria/reactive/eventsourcing/repo/CommandPublisher.java
+++
@@ -1,8 +0,0 @@
-package io.memoria.reactive.eventsourcing.repo;
-
-import io.memoria.atom.eventsourcing.Command;
-import io.vavr.control.Try;
-
-public interface CommandPublisher<C extends Command> {
-  Try<C> publish(C cmd);
-}
--- testsuite/src/main/java/io/memoria/reactive/testsuite/Main.java
+++
@@ -1,7 +0,0 @@
-package io.memoria.reactive.testsuite;
-
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("hello");
-  }
-}
