67729cb8079cdcc6b31ad78948d45a0f5045fb2a
Ismail Marmoush <marmoushismail@gmail.com>
2023-12-21 14:11:11 +0100

    Message bus (#8)

---
+++ kafka/src/main/java/io/memoria/active/kafka/KafkaStream.java
@@ -0,0 +1,67 @@
+package io.memoria.active.kafka;
+
+import io.memoria.atom.core.stream.BlockingStream;
+import io.memoria.atom.core.stream.Msg;
+import io.vavr.collection.Map;
+import io.vavr.collection.Stream;
+import io.vavr.control.Try;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.TopicPartition;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static io.memoria.active.kafka.KafkaUtils.toRecord;
+
+public class KafkaStream implements BlockingStream {
+  public final Map<String, Object> producerConfig;
+  public final Map<String, Object> consumerConfig;
+  private final Duration timeout;
+  private final KafkaProducer<String, String> kafkaProducer;
+  private final List<KafkaConsumer<String, String>> kafkaConsumers;
+
+  public KafkaStream(Map<String, Object> producerConfig, Map<String, Object> consumerConfig, Duration timeout) {
+    this.producerConfig = producerConfig;
+    this.consumerConfig = consumerConfig;
+    this.timeout = timeout;
+    this.kafkaProducer = new KafkaProducer<>(producerConfig.toJavaMap());
+    this.kafkaConsumers = new ArrayList<>();
+  }
+
+  @Override
+  public Try<Msg> publish(String topic, int partition, Msg msg) {
+    return Try.of(() -> send(topic, partition, msg)).map(meta -> msg);
+  }
+
+  @Override
+  public Try<Stream<Msg>> fetch(String topic, int partition) {
+    return Try.of(() -> consume(topic, partition));
+  }
+
+  @Override
+  public void close() {
+    this.kafkaProducer.close();
+    this.kafkaConsumers.forEach(KafkaConsumer::close);
+  }
+
+  private RecordMetadata send(String topic, int partition, Msg msg)
+          throws InterruptedException, ExecutionException, TimeoutException {
+    return this.kafkaProducer.send(toRecord(topic, partition, msg)).get(timeout.toMillis(), TimeUnit.MILLISECONDS);
+  }
+
+  private Stream<Msg> consume(String topic, int partition) {
+    var consumer = new KafkaConsumer<String, String>(consumerConfig.toJavaMap());
+    this.kafkaConsumers.add(consumer);
+    var tp = new TopicPartition(topic, partition);
+    var tpCol = io.vavr.collection.List.of(tp).toJavaList();
+    consumer.assign(tpCol);
+    consumer.seekToBeginning(tpCol);
+    return KafkaUtils.consume(consumer, tp, timeout).map(KafkaUtils::toMsg);
+  }
+}
---
+++ nats/src/test/java/io/memoria/active/nats/NatsStreamIT.java
@@ -0,0 +1,54 @@
+package io.memoria.active.nats;
+
+import io.memoria.atom.core.stream.Msg;
+import io.nats.client.JetStreamApiException;
+import io.vavr.collection.Stream;
+import io.vavr.control.Try;
+import org.assertj.core.api.Assertions;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import static io.memoria.active.nats.Infra.NATS_CONFIG;
+
+@TestMethodOrder(OrderAnnotation.class)
+class NatsStreamIT {
+  private static final int count = 100;
+  private static final String topic = "commands_" + System.currentTimeMillis();
+  private static final int partition = 0;
+  private static final NatsStream stream;
+  private static boolean await = false;
+
+  static {
+    try {
+      stream = new NatsStream(NATS_CONFIG, Duration.ofMillis(500));
+      NatsUtils.createOrUpdateTopic(NATS_CONFIG, topic, 1);
+    } catch (IOException | InterruptedException | JetStreamApiException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  @Order(0)
+  void publishing() {
+    await = Stream.range(0, count)
+                  .map(String::valueOf)
+                  .map(i -> new Msg(i, i))
+                  .map(msg -> stream.publish(topic, partition, msg))
+                  .map(Try::isSuccess)
+                  .forAll(b -> b);
+    Awaitility.await().timeout(Duration.ofSeconds(3)).until(() -> await);
+  }
+
+  @Test
+  @Order(1)
+  void stream() {
+    var size = stream.fetch(topic, partition).get().take(count).size();
+    Assertions.assertThat(size).isEqualTo(count);
+  }
+}
